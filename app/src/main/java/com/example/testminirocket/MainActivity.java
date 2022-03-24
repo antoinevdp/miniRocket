323d689867ea655a2f10f76274f50226214f5fe8
antoinevdp <antoinevdp@live.fr>
2022-03-24 10:58:12 +0100

    initial commit

---
+++ app/src/main/java/com/example/testminirocket/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.testminirocket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_LONG).show();
+        
+    }
+}
