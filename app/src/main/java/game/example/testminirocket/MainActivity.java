a9b7927bf3aa5cf6f9d8d6cfece84e103be15162
lucas <lucas.mouquet.lm@gmail.com>
2022-03-31 10:38:07 +0200

    main page

--- app/src/main/java/game/example/testminirocket/MainActivity.java
+++ app/src/main/java/game/example/testminirocket/MainActivity.java
@@ -1,43 +1,35 @@
 package game.example.testminirocket;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 import android.widget.Button;
 
 
 import com.minirocket.game.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         setContentView(R.layout.activity_main);
 
-
-        /*getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);*/
         Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_LONG).show();
 
-        button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.btn_play);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, secondActivity.class));
-            }
-        });
+
         
     }
 
