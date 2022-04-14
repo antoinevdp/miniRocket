f30f423f49d4aa1055ff5ec8bc06bca02d6833a7
antoinevdp <antoinevdp@live.fr>
2022-04-14 11:55:25 +0200

    menu galaxie qui tourne

--- app/src/main/java/game/example/testminirocket/MainActivity.java
+++ app/src/main/java/game/example/testminirocket/MainActivity.java
@@ -50,9 +50,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), secondActivity.class);
-                //myIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivityForResult(myIntent, 0);
-                //overridePendingTransition(0,0);
             }
         });
 
--- app/src/main/java/game/example/testminirocket/secondActivity.java
+++ app/src/main/java/game/example/testminirocket/secondActivity.java
@@ -1,16 +1,20 @@
 package game.example.testminirocket;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageButton;
 
 import com.minirocket.game.R;
 
 public class secondActivity extends Activity {
+
+    Animation rotateAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,35 @@ public class secondActivity extends Activity {
         );
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        //button retour
+        ImageButton button = (ImageButton) findViewById(R.id.returnButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        ImageButton galaxie1 = (ImageButton) findViewById(R.id.galaxie1);
+        ImageButton galaxie2 = (ImageButton) findViewById(R.id.galaxie2);
+        ImageButton galaxie3 = (ImageButton) findViewById(R.id.galaxie3);
+        ImageButton galaxie4 = (ImageButton) findViewById(R.id.galaxie4);
+
+
+        rotateAnimation(galaxie1,7000) ;
+        rotateAnimation(galaxie2,5000) ;
+        rotateAnimation(galaxie3,4000) ;
+        rotateAnimation(galaxie4,6000) ;
+
+    }
+
+    private void rotateAnimation(ImageButton imageButton, long duration){
+        rotateAnimation= AnimationUtils.loadAnimation(this,R.anim.rotate);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setDuration(duration);
+        imageButton.startAnimation(rotateAnimation);
     }
 
     @Override
