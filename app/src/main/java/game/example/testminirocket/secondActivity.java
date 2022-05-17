8ec757cc0d6c2faee8f53737c7145b9b383940a1
antoinevdp <antoinevdp@live.fr>
2022-05-17 19:44:31 +0200

    menu

--- app/src/main/java/game/example/testminirocket/secondActivity.java
+++ app/src/main/java/game/example/testminirocket/secondActivity.java
@@ -1,6 +1,13 @@
 package game.example.testminirocket;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -9,12 +16,15 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.minirocket.game.R;
 
 public class secondActivity extends Activity {
 
     Animation rotateAnimation;
+    int selectLvl = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +54,96 @@ public class secondActivity extends Activity {
         ImageButton galaxie3 = (ImageButton) findViewById(R.id.galaxie3);
         ImageButton galaxie4 = (ImageButton) findViewById(R.id.galaxie4);
 
+        ImageView traj1 = (ImageView) findViewById(R.id.imageView4);
+        ImageView traj2 = (ImageView) findViewById(R.id.imageView3);
+        ImageView traj3 = (ImageView) findViewById(R.id.imageView5);
+        ImageView traj4 = (ImageView) findViewById(R.id.imageView6);
+
+        ImageView rocket = (ImageView) findViewById(R.id.rocketbutton);
+        ImageButton cerclebutton = (ImageButton) findViewById(R.id.cercle);
+
 
         rotateAnimation(galaxie1,7000) ;
         rotateAnimation(galaxie2,5000) ;
         rotateAnimation(galaxie3,4000) ;
         rotateAnimation(galaxie4,6000) ;
 
+        //line between planet and rocket
+
+        galaxie1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                traj1.setVisibility(View.VISIBLE);
+                traj2.setVisibility(View.INVISIBLE);
+                traj3.setVisibility(View.INVISIBLE);
+                traj4.setVisibility(View.INVISIBLE);
+                rocket.setImageResource(R.drawable.ic_round_rocket_green);
+                cerclebutton.setImageResource(R.drawable.ic_ellipse_2_green);
+                selectLvl = 1;
+            }
+        });
+        galaxie2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                traj2.setVisibility(View.VISIBLE);
+                traj1.setVisibility(View.INVISIBLE);
+                traj3.setVisibility(View.INVISIBLE);
+                traj4.setVisibility(View.INVISIBLE);
+                rocket.setImageResource(R.drawable.ic_round_rocket_green);
+                cerclebutton.setImageResource(R.drawable.ic_ellipse_2_green);
+                selectLvl = 2;
+            }
+        });
+        galaxie3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                traj3.setVisibility(View.VISIBLE);
+                traj1.setVisibility(View.INVISIBLE);
+                traj2.setVisibility(View.INVISIBLE);
+                traj4.setVisibility(View.INVISIBLE);
+                rocket.setImageResource(R.drawable.ic_round_rocket_green);
+                cerclebutton.setImageResource(R.drawable.ic_ellipse_2_green);
+                selectLvl = 3;
+            }
+        });
+        galaxie4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                traj4.setVisibility(View.VISIBLE);
+                traj1.setVisibility(View.INVISIBLE);
+                traj2.setVisibility(View.INVISIBLE);
+                traj3.setVisibility(View.INVISIBLE);
+                rocket.setImageResource(R.drawable.ic_round_rocket_green);
+                cerclebutton.setImageResource(R.drawable.ic_ellipse_2_green);
+                selectLvl = 4;
+            }
+        });
+
+        cerclebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), MainActivityTest.class);
+                switch (selectLvl){
+                    case  0:
+                        Toast.makeText(secondActivity.this, "Choose a level", Toast.LENGTH_SHORT).show();
+                        break;
+                    case  1:
+                        startActivityForResult(myIntent, 0);
+                        break;
+                    case  2:
+                        startActivityForResult(myIntent, 0);
+                        break;
+                    case  3:
+                        startActivityForResult(myIntent, 0);
+                        break;
+                    case  4:
+                        startActivityForResult(myIntent, 0);
+                        break;
+                }
+            }
+        });
     }
+
 
     private void rotateAnimation(ImageButton imageButton, long duration){
         rotateAnimation= AnimationUtils.loadAnimation(this,R.anim.rotate);
@@ -59,6 +152,7 @@ public class secondActivity extends Activity {
         imageButton.startAnimation(rotateAnimation);
     }
 
+    //no transition between activity
     @Override
     public void onPause() {
         super.onPause();
